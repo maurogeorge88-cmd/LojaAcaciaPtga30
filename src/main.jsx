8488aa224abb539b561d8a1d96f274654f175453
maurogeorge88-cmd <maurogeorge88@gmail.com>
2025-11-25 05:43:34 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App />  {/* ← Voltar para App */}
   </React.StrictMode>,
 )
