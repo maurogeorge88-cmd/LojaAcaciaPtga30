71fe861ece5098f63712ba35a10d1b751113b221
maurogeorge88-cmd <maurogeorge88@gmail.com>
2025-11-25 05:42:32 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { TestComponents } from './TestComponents.jsx'  // ← ADICIONAR
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TestComponents />  {/* ← TROCAR App por TestComponents */}
+    <App />
   </React.StrictMode>,
 )
