24ab075de75ffe9ddc2e2e42da86e85a0979357b
maurogeorge88-cmd <maurogeorge88@gmail.com>
2025-12-01 11:51:11 -0400

    Update FinancasLoja.jsx

--- src/components/financeiro/FinancasLoja.jsx
+++ src/components/financeiro/FinancasLoja.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../../App';
+import { supabase } from '../../supabaseClient';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
 
