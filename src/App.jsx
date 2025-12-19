17ea9f4977153c849ccf342add2a288b143ebde9
maurogeorge88-cmd <maurogeorge88@gmail.com>
2025-12-19 16:51:48 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -285,20 +285,20 @@ function App() {
       
       // Definir permissões baseadas no nível de acesso
       if (data.nivel_acesso === 'irmao') {
-        // Irmão comum: apenas visualização
+        // Irmão: usa permissões específicas do banco
         setPermissoes({
-          canEdit: false,
-          canEditMembers: false,
+          canEdit: data.pode_editar_cadastros || false,
+          canEditMembers: data.pode_editar_cadastros || false,
           canDelete: false,
           canManageUsers: false,
-          canViewFinancial: false,
-          canEditFinancial: false,
-          pode_editar_biblioteca: false,
-          pode_editar_comodatos: false,
-          pode_editar_caridade: false,
-          pode_editar_balaustres: false,
-          pode_editar_pranchas: false,
-          pode_editar_comissoes: false,
+          canViewFinancial: data.pode_visualizar_financeiro || false,
+          canEditFinancial: data.pode_editar_financeiro || false,
+          pode_editar_biblioteca: data.pode_editar_biblioteca || false,
+          pode_editar_comodatos: data.pode_editar_comodatos || false,
+          pode_editar_caridade: data.pode_editar_caridade || false,
+          pode_editar_balaustres: data.pode_editar_balaustres || false,
+          pode_editar_pranchas: data.pode_editar_pranchas || false,
+          pode_editar_comissoes: data.pode_editar_comissoes || false,
           pode_editar_corpo_admin: false
         });
       } else if (data.nivel_acesso === 'admin') {
