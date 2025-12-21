788e6fa50b40a9d28c1382010e05b8e18348b3d0
maurogeorge88-cmd <maurogeorge88@gmail.com>
2025-12-21 06:35:27 -0400

    Fix formatting in textoComodato string

--- src/components/comodatos/GestaoEmprestimos.jsx
+++ src/components/comodatos/GestaoEmprestimos.jsx
@@ -403,7 +403,7 @@ export default function GestaoEmprestimos({ showSuccess, showError, permissoes }
       // ========================================
       doc.setFont('helvetica', 'normal');
       
-      const textoComodato = `Este  Termo  de  Comodato  estabelece as condições do empréstimo gratuito do(s) bem(s) descrito(s)   acima,  o(s) qual(is)   é(são)    disponibilizado(s)    pela    Loja    Maçônica   -   ARLS   Acácia   de   Paranatinga   nº   30,   para   que   seja(m)    utilizado(s)    pelo    beneficiário   acima   identificado,  sendo  vedada  a transferência  à  terceiros   sem  a autorização do cedente.
+      const textoComodato = `Este  Termo  de  Comodato  estabelece as condições do empréstimo gratuito do(s) bem(s) descrito(s)   acima,  o(s) qual(is)   é(são)    disponibilizado(s)    pela    Loja    Maçônica   -   ARLS   Acácia   de   Paranatinga   nº    30,   para   que   seja(m)    utilizado(s)    pelo    beneficiário   acima   identificado,  sendo  vedada  a  transferência  à  terceiros   sem  a autorização do cedente.
 
 O beneficiário deve cuidar do(s) bem(s) disponibilizado(s) e devolvê-lo(s) em boas condições para uso  posterior,  e será  responsabilizado  por quaisquer danos ou perda.
 
