a712d1af929b1eaedb642f917967b4b4376ff953
maurogeorge88-cmd <maurogeorge88@gmail.com>
2025-12-24 09:18:37 -0400

    Update CadastrarIrmao.jsx

--- src/components/irmaos/CadastrarIrmao.jsx
+++ src/components/irmaos/CadastrarIrmao.jsx
@@ -1413,7 +1413,7 @@ const CadastrarIrmao = ({ irmaos, irmaoParaEditar, onUpdate, showSuccess, showEr
                     </label>
                     <input
                       type="date"
-                      value={pais.data_nascimento}
+                      value={pais.data_nascimento_mae}
                       onChange={(e) => setPais({ ...pais, data_nascimento_mae: e.target.value })}
                       className="w-full px-3 py-2 border border-gray-300 rounded focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                     />
