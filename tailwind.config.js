5af64046c3d6d572887fbb7751ce85fe6dbf452b
maurogeorge88-cmd <maurogeorge88@gmail.com>
2026-01-04 14:03:40 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,29 +8,19 @@ export default {
     extend: {
       colors: {
         primary: {
-          50: 'rgb(var(--color-primary-50) / <alpha-value>)',
-          100: 'rgb(var(--color-primary-100) / <alpha-value>)',
-          200: 'rgb(var(--color-primary-200) / <alpha-value>)',
-          300: 'rgb(var(--color-primary-300) / <alpha-value>)',
-          400: 'rgb(var(--color-primary-400) / <alpha-value>)',
-          500: 'rgb(var(--color-primary-500) / <alpha-value>)',
-          600: 'rgb(var(--color-primary-600) / <alpha-value>)',
-          700: 'rgb(var(--color-primary-700) / <alpha-value>)',
-          800: 'rgb(var(--color-primary-800) / <alpha-value>)',
-          900: 'rgb(var(--color-primary-900) / <alpha-value>)',
+          50: 'rgb(var(--color-primary-50))',
+          100: 'rgb(var(--color-primary-100))',
+          200: 'rgb(var(--color-primary-200))',
+          300: 'rgb(var(--color-primary-300))',
+          400: 'rgb(var(--color-primary-400))',
+          500: 'rgb(var(--color-primary-500))',
+          600: 'rgb(var(--color-primary-600))',
+          700: 'rgb(var(--color-primary-700))',
+          800: 'rgb(var(--color-primary-800))',
+          900: 'rgb(var(--color-primary-900))',
         },
       },
     },
-  },
-  plugins: [],
-}/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
   },
   plugins: [],
 }
