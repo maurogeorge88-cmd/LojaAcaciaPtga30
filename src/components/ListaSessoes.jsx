e062fba414bfccebbafd94806206d8932f0b8ddd
maurogeorge88-cmd <maurogeorge88@gmail.com>
2026-01-22 10:06:08 -0400

    Update ListaSessoes.jsx

--- src/components/ListaSessoes.jsx
+++ src/components/ListaSessoes.jsx
@@ -181,7 +181,7 @@ export default function ListaSessoes({ onEditarPresenca, onVisualizarPresenca, o
             mestres_instalados: mestresInstalados
           }
         };
-      }) || []);
+      }));
       
       setSessoes(sessoesComPresenca);
 
