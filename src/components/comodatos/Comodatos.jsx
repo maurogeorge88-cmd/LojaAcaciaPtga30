20f75647ac11d83dd763cb75089f80347b32791e
maurogeorge88-cmd <maurogeorge88@gmail.com>
2026-03-22 19:45:54 -0300

    Update Comodatos.jsx

--- src/components/comodatos/Comodatos.jsx
+++ src/components/comodatos/Comodatos.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../../App';
+import { supabase } from '../../supabaseClient';
 import GestaoEquipamentos from './GestaoEquipamentos';
 import GestaoBeneficiarios from './GestaoBeneficiarios';
 import GestaoEmprestimos from './GestaoEmprestimos';
